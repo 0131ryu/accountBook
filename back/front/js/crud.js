d667cb56f5d8a5f61cdb912ab405a7127452d4fa
0131ryu <131ryuji@gmail.com>
2022-08-09 17:55:32 +0900

    수정, 삭제, 단어 카운팅함

--- back/front/js/crud.js
+++ back/front/js/crud.js
@@ -14,14 +14,58 @@ async function readWords() {
   try {
     const res = await axios(config);
     const wordDataSet = res.data.result;
-    // console.log(wordDataSet);
+    console.log(wordDataSet);
+
+    //단어 개수
+    const easyCount = Object.keys(wordDataSet.easy).length;
+    const middleCount = Object.keys(wordDataSet.middle).length;
+    const advanceCount = Object.keys(wordDataSet.advance).length;
+
+    let checkedCount = 0;
+
+    for (let i = 0; i < easyCount; i++) {
+      const easyCheckedCount = wordDataSet.easy[i].status;
+      console.log(easyCheckedCount);
+      if (easyCheckedCount === "C") {
+        checkedCount += 1;
+      }
+    }
+
+    for (let i = 0; i < middleCount; i++) {
+      const middleCheckedCount = wordDataSet.middle[i].status;
+      console.log(middleCheckedCount);
+      if (middleCheckedCount === "C") {
+        checkedCount += 1;
+      }
+    }
+
+    for (let i = 0; i < advanceCount; i++) {
+      const advanceCheckedCount = wordDataSet.advance[i].status;
+      console.log(advanceCheckedCount);
+      if (advanceCheckedCount === "C") {
+        checkedCount += 1;
+      }
+    }
+    console.log(checkedCount);
+
+    //총 단어 개수
+    const allCount = easyCount + middleCount + advanceCount;
+    console.log(allCount);
+
+    const $checkedCountingAll = document.querySelector(
+      `#word-counting-checked`
+    );
+    const $wordCountingAll = document.querySelector(`#word-counting-all`);
+
+    $wordCountingAll.innerHTML = allCount;
+    $checkedCountingAll.innerHTML = checkedCount;
 
     for (let section in wordDataSet) {
       // console.log(section);
 
       const $sectionUl = document.querySelector(`#${section} ul`);
       const arrayForEachSection = wordDataSet[section];
-      // console.log(arrayForEachSection);
+      console.log(arrayForEachSection);
 
       let result = "";
       for (let word of arrayForEachSection) {
@@ -68,9 +112,9 @@ function cudController(event) {
   const className = target.className;
   const eventType = event.type;
   const key = event.key;
-  console.log(target.className);
-  console.log(eventType);
-  console.log(key);
+  // console.log(target.className);
+  // console.log(eventType);
+  // console.log(key);
   //create
   if (className === "matrix-input-btn-easy" && eventType === "click") {
     createWordsEasy(event, token);
@@ -86,6 +130,24 @@ function cudController(event) {
     createWordsAdvance(event, token);
     return;
   }
+  //update - 체크박스
+  if (className === "word-done" && eventType === "click") {
+    updateWordDone(event, token);
+    return;
+  }
+
+  //update - 수정하기
+  const firstClassName = target.className.split(" ")[0];
+  if (firstClassName === "word-update" && eventType === "click") {
+    updateWordList(event, token);
+    return;
+  }
+
+  //delete - 삭제하기
+  if (firstClassName === "word-delete" && eventType === "click") {
+    deleteWord(event, token);
+    return;
+  }
 }
 
 async function createWordsEasy(event, token) {
@@ -94,10 +156,10 @@ async function createWordsEasy(event, token) {
 
   const english = $english.value;
   const korean = $korean.value;
-  console.log("english", english);
-  console.log("korean", korean);
+  // console.log("english", english);
+  // console.log("korean", korean);
   const type = event.target.closest(".matrix-item").id;
-  console.log(type);
+  // console.log(type);
 
   if (!english || !korean) {
     alert("영어, 한글 단어를 입력해주세요");
@@ -134,10 +196,10 @@ async function createWordsMiddle(event, token) {
 
   const english = $english.value;
   const korean = $korean.value;
-  console.log("english", english);
-  console.log("korean", korean);
+  // console.log("english", english);
+  // console.log("korean", korean);
   const type = event.target.closest(".matrix-item").id;
-  console.log(type);
+  // console.log(type);
 
   if (!english || !korean) {
     alert("영어, 한글 단어를 입력해주세요");
@@ -174,10 +236,10 @@ async function createWordsAdvance(event, token) {
 
   const english = $english.value;
   const korean = $korean.value;
-  console.log("english", english);
-  console.log("korean", korean);
+  // console.log("english", english);
+  // console.log("korean", korean);
   const type = event.target.closest(".matrix-item").id;
-  console.log(type);
+  // console.log(type);
 
   if (!english || !korean) {
     alert("영어, 한글 단어를 입력해주세요");
@@ -205,5 +267,92 @@ async function createWordsAdvance(event, token) {
     return true;
   } catch (err) {
     console.error(err);
+  }
+}
+
+async function updateWordDone(event, token) {
+  // console.log(event.target.checked);
+  const status = event.target.checked ? "C" : "A";
+  const wordIdx = event.target.closest(".list-item").id;
+
+  //console.log(wordIdx);
+  const config = {
+    method: "patch",
+    url: url + "/word",
+    headers: { "w-access-token": token },
+    data: {
+      wordIdx: wordIdx,
+      status: status,
+    },
+  };
+  try {
+    const res = await axios(config);
+    if (res.data.code !== 200) {
+      alert(res.data.message);
+      return false;
+    }
+    readWords();
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+async function updateWordList(event, token) {
+  const english = prompt("수정할 영어단어를 입력하세요");
+  const korean = prompt("수정할 단어 뜻을 입력하세요");
+
+  const wordIdx = event.target.closest(".list-item").id;
+
+  //console.log(wordIdx);
+  const config = {
+    method: "patch",
+    url: url + "/word",
+    headers: { "w-access-token": token },
+    data: {
+      wordIdx: wordIdx,
+      english: english,
+      korean: korean,
+    },
+  };
+  try {
+    const res = await axios(config);
+    if (res.data.code !== 200) {
+      alert(res.data.message);
+      return false;
+    }
+    //DOM 업데이트
+    readWords();
+  } catch (err) {
+    console.error(err);
+    return false;
+  }
+}
+
+async function deleteWord(event, token) {
+  const isValidReq = confirm(
+    "삭제하시겠습니까? 삭제 후에는 복구가 어렵습니다."
+  );
+  const wordIdx = event.target.closest(".list-item").id;
+
+  if (!isValidReq) {
+    return false;
+  }
+
+  const config = {
+    method: "delete",
+    url: url + `/word/${wordIdx}`,
+    headers: { "w-access-token": token },
+  };
+  try {
+    const res = await axios(config);
+    if (res.data.code !== 200) {
+      alert(res.data.message);
+      return false;
+    }
+    //DOM 업데이트
+    readWords();
+  } catch (err) {
+    console.error(err);
+    return false;
   }
 }
