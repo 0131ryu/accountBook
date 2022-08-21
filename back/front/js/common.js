6f48a128bad1455a1d14e7bc0e408fbd5b6891d1
0131ryu <131ryuji@gmail.com>
2022-08-21 13:57:05 +0900

    delete-result 수정

--- back/front/js/common.js
+++ back/front/js/common.js
@@ -1,3 +1,3 @@
 // const url = "http://127.0.0.1:3000";
-const url = "https://www.engword.shop";
-// const url = "http://127.0.0.1:4000";
+// const url = "https://www.engword.shop";
+const url = "http://127.0.0.1:4000";
