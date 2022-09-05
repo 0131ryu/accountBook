3458a4b894c41cbd21afdfa8f7433c9fd782039d
0131ryu <131ryuji@gmail.com>
2022-09-05 15:20:15 +0900

    req.user에 따라 index.html 조건 달라지기

--- back/src/router/pageRouter.js
+++ back/src/router/pageRouter.js
@@ -16,123 +16,114 @@ router.use((req, res, next) => {
   next();
 });
 
-router.get("/", async (req, res, next) => {
-  try {
-    const words = await Word.findAll({
-      include: {
-        model: User,
-        attributes: ["id", "nickname"],
-      },
-      order: [["createdAt", "DESC"]],
-    });
-    res.render("index", {
-      title: "engWord",
-      words: words,
-    });
-  } catch (error) {
-    console.error(error);
-    next(error);
-  }
-});
-
 //영단어
 router.get("/index", async (req, res, next) => {
-  //유저 아이디 값을 params로 넘겨서 받아 개수로 받으면 어떨지?
-  //post 결과 보려면 이 부분 넣어야 함
   try {
-    const words = await Word.findAll({
-      include: {
-        model: User,
-        attributes: ["id", "nickname"],
-      },
-      order: [["createdAt", "DESC"]],
-    });
-    const wordsEasy = await Word.findAll({
-      include: {
-        model: User,
-        attributes: ["id"],
-      },
-      attributes: ["id", "english", "korean", "status", "UserId"],
-      where: {
-        [Op.or]: [{ status: "A" }, { status: "C" }],
-        type: "easy",
-      },
-      order: [["createdAt", "DESC"]],
-    });
-    const wordsMiddle = await Word.findAll({
-      include: {
-        model: User,
-        attributes: ["id"],
-      },
-      attributes: ["id", "english", "korean", "status", "UserId"],
-      where: {
-        [Op.or]: [{ status: "A" }, { status: "C" }],
-        type: "middle",
-      },
-      order: [["createdAt", "DESC"]],
-    });
-    const wordsAdvance = await Word.findAll({
-      include: {
-        model: User,
-        attributes: ["id"],
-      },
-      attributes: ["id", "english", "korean", "status", "UserId"],
-      where: {
-        [Op.or]: [{ status: "A" }, { status: "C" }],
-        type: "advance",
-      },
-      order: [["createdAt", "DESC"]],
-    });
-    //inner join
-    const total = await Word.count({
-      include: {
-        model: User,
-        attributes: ["id"],
-      },
-      where: {
-        UserId: req.user.id,
-        // UserId: {
-        //   [Op.or]: [0, req.user.id],
-        // },
-        // [Op.or]: [{ UserId: undefined }, { UserId: req.user.id }],
-      },
-    });
-    const counting = await Word.count({
-      include: {
-        model: User,
-        attributes: ["id"],
-      },
-      where: {
-        status: "C",
-        UserId: req.user.id,
-        // UserId: {
-        //   [Op.or]: [0, req.user.id],
-        // },
-        // [Op.or]: [{ UserId: undefined }, { UserId: req.user.id }],
-      },
-    });
-    const deletedWord = await Word.findAll({
-      include: {
-        model: User,
-        attributes: ["id"],
-      },
-      attributes: ["id", "english", "korean", "type"],
-      where: {
-        status: "D",
-      },
-      order: [["createdAt", "DESC"]],
-    });
-    console.log("counting", counting);
-    res.render("index", {
-      title: "engWord",
-      words: words,
-      wordsEasy: wordsEasy,
-      wordsMiddle: wordsMiddle,
-      wordsAdvance: wordsAdvance,
-      total: total,
-      counting: counting,
-      deletedWord: deletedWord,
-    });
+    if (req.user) {
+      const words = await Word.findAll({
+        include: {
+          model: User,
+          attributes: ["id", "nickname"],
+        },
+        order: [["createdAt", "DESC"]],
+      });
+      const wordsEasy = await Word.findAll({
+        include: {
+          model: User,
+          attributes: ["id"],
+        },
+        attributes: ["id", "english", "korean", "status", "UserId"],
+        where: {
+          [Op.or]: [{ status: "A" }, { status: "C" }],
+          type: "easy",
+        },
+        order: [["createdAt", "DESC"]],
+      });
+      const wordsMiddle = await Word.findAll({
+        include: {
+          model: User,
+          attributes: ["id"],
+        },
+        attributes: ["id", "english", "korean", "status", "UserId"],
+        where: {
+          [Op.or]: [{ status: "A" }, { status: "C" }],
+          type: "middle",
+        },
+        order: [["createdAt", "DESC"]],
+      });
+      const wordsAdvance = await Word.findAll({
+        include: {
+          model: User,
+          attributes: ["id"],
+        },
+        attributes: ["id", "english", "korean", "status", "UserId"],
+        where: {
+          [Op.or]: [{ status: "A" }, { status: "C" }],
+          type: "advance",
+        },
+        order: [["createdAt", "DESC"]],
+      });
+      const total = await Word.count({
+        include: {
+          model: User,
+          attributes: ["id"],
+        },
+        where: {
+          UserId: req.user.id,
+          // UserId: {
+          //   [Op.or]: [0, req.user.id],
+          // },
+          // [Op.or]: [{ UserId: undefined }, { UserId: req.user.id }],
+        },
+      });
+      const counting = await Word.count({
+        include: {
+          model: User,
+          attributes: ["id"],
+        },
+        where: {
+          status: "C",
+          UserId: req.user.id,
+          // UserId: {
+          //   [Op.or]: [0, req.user.id],
+          // },
+          // [Op.or]: [{ UserId: undefined }, { UserId: req.user.id }],
+        },
+      });
+      const deletedWord = await Word.findAll({
+        include: {
+          model: User,
+          attributes: ["id"],
+        },
+        attributes: ["id", "english", "korean", "type"],
+        where: {
+          status: "D",
+        },
+        order: [["createdAt", "DESC"]],
+      });
+      res.render("index", {
+        title: "engWord",
+        words: words,
+        wordsEasy: wordsEasy,
+        wordsMiddle: wordsMiddle,
+        wordsAdvance: wordsAdvance,
+        total: total,
+        counting: counting,
+        deletedWord: deletedWord,
+      });
+    } else {
+      const words = await Word.findAll({
+        include: {
+          model: User,
+          attributes: ["id", "nickname"],
+        },
+        order: [["createdAt", "DESC"]],
+      });
+      res.render("index", {
+        title: "engWord",
+        words: words,
+      });
+    }
   } catch (error) {
     console.error(error);
     next(error);
