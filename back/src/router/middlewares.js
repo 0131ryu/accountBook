e0cb4851993290c80d60eca23d560022f333be04
0131ryu <131ryuji@gmail.com>
2022-09-06 12:04:12 +0900

    index.html axios 전 조건 수정

--- back/src/router/middlewares.js
+++ back/src/router/middlewares.js
@@ -2,7 +2,9 @@ exports.isLoggedIn = (req, res, next) => {
   if (req.isAuthenticated()) {
     next();
   } else {
-    res.status(403).send("로그인 필요");
+    res.send(
+      "<script>alert('로그인이 필요합니다.');location.href='/login';</script>"
+    );
   }
 };
 
@@ -11,6 +13,6 @@ exports.isNotLoggedIn = (req, res, next) => {
     next();
   } else {
     const message = encodeURIComponent("로그인한 상태입니다.");
-    res.redirect(`/main?error=${message}`);
+    res.redirect(`/index?error=${message}`);
   }
 };
